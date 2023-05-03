5f637ce949273afe25c0d5c445e84d399861b7ee
Banwind <a84012807@gmail.com>
2023-05-03 13:12:56 +0800

    feat: add add.js

--- app.js
+++ app.js
@@ -7,4 +7,38 @@ const methodOverride = require('method-override')
 const routes = require('./routes') // 引用路由器
 const flash = require('connect-flash')
 const app = express()
+const PORT = process.env.PORT
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+require('./config/mongoose')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false, // 當設定為 true 時，會在每一次與使用者互動後，強制把 session 更新到 session store 裡。
+  saveUninitialized: true // 強制將未初始化的 session 存回 session store。未初始化表示這個 session 是新的而且沒有被修改過，例如未登入的使用者的 session。
+}))
+
+app.use(express.urlencoded({ extended: true}))
+app.use(methodOverride('_method'))
+
+usePassport(app)
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user 
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+})
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
