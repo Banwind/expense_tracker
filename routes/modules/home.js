c92b685d2690ea8db44a147264f70590d3d4a3c7
Banwind <a84012807@gmail.com>
2023-05-08 17:17:58 +0800

    feat: modify search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
-const Category = require('../../models/category')
-const categoryJSON = require('../../models/seeds/category.json')
 
 router.get('/', async (req, res, next) => {
   const userId = req.user._id
-  
+
   Record.find({ userId })
     .lean()
     .then(records => {
@@ -19,6 +17,5 @@ router.get('/', async (req, res, next) => {
     })
     .catch(error => console.error(error))
 })
-
 
 module.exports = router
--- routes/modules/search.js
+++ routes/modules/search.js
@@ -1,26 +1,37 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
-const Category = require('../../models/category')
+const categoryModel = require('../../models/category')
 
-router.get('/:category?', async (req, res) => {
+router.get('/:category?', async (req, res, next) => {
   const userId = req.user._id
-  const { category: categoryEnName } = req.params
+  let categoryId = req.params.category
 
-  let filter = { userId }
-  if (categoryEnName) {
-    const category = await Category.findOne({ name_en: categoryEnName }).lean()
-    filter.category = category._id
+  try {
+    const category = await categoryModel.findOne({ name_en: categoryId }).lean();
+
+    if (category) {
+      categoryId = category._id;
+    
+      Record.find({ userId, categoryId })
+        .lean()
+        .then(records => {
+          const total_cost = records.reduce((sum, record) => sum + record.amount, 0)
+          records = records.map(record => {
+            record.date = new Date(record.date).toLocaleDateString()
+            return record
+          })
+          res.render('index', { records, total_cost })
+        })
+        .catch(error => console.error(error))
+    } else {
+      res.redirect('/')
+    }
+  } catch (error) {
+    // 捕獲查詢過程中可能出現的錯誤
+    console.error(error)
   }
 
-  Record.find(filter)
-    .populate('category')
-    .lean()
-    .then(records => {
-      const totalAmount = records.reduce((sum, record) => sum + record.amount, 0)
-      res.render('index', { records, totalAmount })
-    })
-    .catch(error => console.error(error))
 })
 
 module.exports = router
